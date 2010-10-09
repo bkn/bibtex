eaff5f117aa52590693e6fc8293e1ee85f6f1dab
Jack Alves <jack.alves@gmail.com>
2010-10-09 11:04:48 -0700

    added conversion to CSV

--- bibtex.js
+++ bibtex.js
@@ -11,8 +11,89 @@ function show_json(response){
 
 }
 
+function get_user_input() {
+	var params = '';
+	var user_input = '';
+	user_input = $('#bibtex_data').val();
+	params = '&document='+encodeURIComponent(user_input);
+	if (!user_input) {
+		user_input = $('#bibtex_url').val();
+		params = '&url='+encodeURIComponent(user_input);
+	}
+	return params;
+}
+
+function get_attributes(response) {
+	var facets = {}
+	for (var i=0; i < response.length; i++) {
+		for(var r in response[i]) {
+			if (r in facets) {
+				facets[r]++;				
+			}
+			else {
+				facets[r] = 1;
+			}
+		}
+	}
+	return facets;
+}
+
+function show_csv(response) {
+	var facets = get_attributes(response);
+	var headers = [];
+	var i = 0;
+	var x = 0;
+	var k;
+	var content = '';
+	for (k in facets) {
+		headers[i] = k;
+		i++;
+	}
+	for (x=0; x < headers.length; x++) {
+		if (x != 0) {
+			content += ',';
+		}
+		content += headers[x];
+	}
+	content += '\n';
+	
+	for (i=0; i < response.length; i++) {
+		for (x=0; x < headers.length; x++) {
+			if (x != 0) {
+				content += ',';
+			}
+			k = headers[x];
+			if ((k in response[i]) && response[i][k]) {
+				content += '"'+response[i][k]+'"';				
+			}
+		}
+		content += '\n';
+	}	
+	$('#csv_data').html(content);
+}
+
+function show_csv_area() {
+	var content = '';
+	$('#csv_area').html("");
+	$('#csv_area').append("<div class='block_header'>CSV Document</div>");
+	content = "<form><div id='csv_form' class='csv_form'></div></form>"; 	
+	$('#csv_area').append(content);
+//	content = "<form>Show as: ";
+//	content += "  <input type='button' value='Table' id='csv_table_button' class='_button'/>";
+//	content += "  <input type='button' value='Text' id='csv_text_button' class='_button'/>";
+//	content += "</form>";
+//	$('#csv_form').html(content);
+	content = '<textarea id="csv_data" class="csv_data" type="textarea" value=""> </textarea>';
+	$('#csv_form').append(content);
+}
+
 function show_bibjson(response) {
 	$('#json_data').html(formattedJSON(response,'file'));
+}
+
+function show_conversion(response) {
+	show_bibjson(response);
+	show_csv(response);
 }
 
 function show_bibtex_area() {
@@ -21,8 +102,14 @@ function show_bibtex_area() {
 	$('#bibtex_area').append("<div class='block_header'>BibTeX Document</div>");
 	content = "<form><div id='bibtex_form' class='bibtex_form'></div></form>"; 	
 	$('#bibtex_area').append(content);
-	content = "<form><input type='button' value='Convert' id='bibtex_convert_button' class='_button'/></form>";
+	
+	content = "<form>Convert to: ";
+	content += "  <input type='button' value='Convert' id='convert_button' class='_button'/>";
+//	content += "  <input type='button' value='JSON' id='bibtex_json_button' class='_button'/>";
+//	content += "  <input type='button' value='CSV' id='bibtex_csv_button' class='_button'/>";
+	content += "</form>";
 	$('#bibtex_form').append(content);
+	
 	content = '<div>Enter a URL:</div>';	
 	content += '<div><input id="bibtex_url" class="bibtex_url" type="text" value="" /></div>';	
 	$('#bibtex_form').append(content);
@@ -33,21 +120,19 @@ function show_bibtex_area() {
 	content += '></textarea></div>';
 	$('#bibtex_form').append(content);
 
-	$('#bibtex_convert_button').click(function () {
-		var params = '';
-		var user_input = '';
-		user_input = $('#bibtex_data').val();
-		params = '&document='+encodeURIComponent(user_input);
-//		deb('data:'+user_input);
-		
-		if (!user_input) {
-			user_input = $('#bibtex_url').val();
-			params = '&url='+encodeURIComponent(user_input);
-//			deb('url params:'+params);
-		}
+	$('#convert_button').click(function () {
+		$('#csv_data').html('');
+		$('#json_data').html('');
 
-		bibtex_service(show_bibjson,params);
+		bibtex_service(show_conversion,get_user_input());
 		});			
+//	$('#bibtex_json_button').click(function () {
+//		bibtex_service(show_bibjson,get_user_input());
+//		});			
+//	$('#bibtex_csv_button').click(function () {
+//		bibtex_service(show_csv,get_user_input());
+//		});			
+
 }
 
 function show_json_area() {
@@ -74,6 +159,7 @@ function show_template_page () {
 	$('body').append("<div id='info_area' class='info_area'></div>");
 	$('body').append("<div id='bibtex_area' class='bibtex_area'></div>");
 	$('body').append("<div id='json_area' class='json_area'></div>");
+	$('body').append("<div id='csv_area' class='csv_area'></div>");
 	
 	$('#page_header_wrapper').html("<div class='logo'>BKN BibTex / JSON Converter</div>");
 	content = "<div class='page_subheader'>";
@@ -86,6 +172,7 @@ function show_template_page () {
 	$('#active_info').append("<div id='status' class='status'></div>");
 	show_bibtex_area();
 	show_json_area();
+	show_csv_area();
 	
 }
 
